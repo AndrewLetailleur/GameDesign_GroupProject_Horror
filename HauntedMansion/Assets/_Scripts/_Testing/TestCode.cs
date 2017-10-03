f9d0473fb86a77a303419afe2572458615e14ae8
Andrew Letailleur <31889767+AndrewLetailleur@users.noreply.github.com>
2017-10-03 13:30:44 +0100

    Add files via upload
    rushed upload archive attemptA

---
+++ HauntedMansion/Assets/_Scripts/_Testing/TestCode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestCode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			SceneManager.LoadScene("GameOver");
+		} else if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			SceneManager.LoadScene("TimeOut");
+		}
+	}
+}
